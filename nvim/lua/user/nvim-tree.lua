98e21be645d95d54abf2a07dee3071e13b787cf8
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2022-07-11 08:43:14 +0200

    resolved the nvim-tree unknown options

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -21,6 +21,32 @@
 --   },
 -- }
 
+--   update_to_buf_dir = {
+--     enable = true,
+--     auto_open = true,
+--   },
+
+
+--     auto_resize = true,
+
+-- quit_on_open = 0,
+
+--  git_hl = 1,
+--  disable_window_picker = 0,
+
+--   show_icons = {
+--     git = 1,
+--     folders = 1,
+--     files = 1,
+--     folder_arrows = 1,
+--     tree_width = 30,
+--   },
+
+-- root_folder_modifier = ":t",
+ 
+
+
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -42,14 +68,9 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
-  auto_close = true,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -82,7 +103,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -97,16 +117,5 @@ nvim_tree.setup {
   trash = {
     cmd = "trash",
     require_confirm = true,
-  },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
   },
 }
