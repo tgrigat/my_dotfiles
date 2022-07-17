b02b278c469cfd445222108b532d3fdd512f6649
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2022-07-17 14:32:45 +0200

    automatic commit

--- plasma-workspace/env/env.sh
+++ plasma-workspace/env/env.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
-
 export GTK_IM_MODULE=fcitx5
 export QT_IM_MODULE=fcitx5
 export XMODIFIERS=@im=fcitx5
 export MATLAB_JAVA=/usr/lib/jvm/java-8-openjdk/jre
-
 # setting environmental variables under wayland
 if [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
     export MOZ_ENABLE_WAYLAND=1
@@ -14,5 +12,7 @@ else
     unset MOZ_ENABLE_WAYLAND
     unset QT_QPA_PLATFORM
 fi
-
 setxkbmap -option caps:escape_shifted_capslock
+
+
+export PATH=~/.local/bin:$PATH
