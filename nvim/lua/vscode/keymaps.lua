04b29927c6377a65b7476d4f56b5aa1eadf22a5a
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2022-10-21 17:06:25 +0800

    Fixed a little bug...

--- nvim/lua/vscode/keymaps.lua
+++ nvim/lua/vscode/keymaps.lua
@@ -1,3 +1,15 @@
+vim.g.mapleader = " "
+
 local keymap = vim.keymap
 
 keymap.set("n", "gr", "<Cmd>call VSCodeNotifyVisual('editor.action.goToReferences',1)<CR>")
+
+keymap.set("n", "<leader>ft", "<Cmd>call VSCodeNotifyVisual('workbench.files.action.focusFilesExplorer',1)<CR>")
+
+keymap.set("n", "<leader>t", "<Cmd>call VSCodeNotifyVisual('workbench.files.action.focusFilesExplorer',1)<CR>")
+
+keymap.set("n", "<C-w>gd", "<Cmd>call VSCodeNotify('editor.action.revealDefinitionAside')<CR>")
+
+keymap.set("n", "?", "<Cmd>call VSCodeNotify('workbench.action.findInFiles', { 'query': expand('<cword>')})<CR>")
+
+
