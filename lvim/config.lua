2c658f78785fa7718745a2a6390d2025b200ca00
tangdi <jiaye.yang@tum.de>
2022-11-07 15:06:27 +0800

    add snippets

--- lvim/config.lua
+++ lvim/config.lua
@@ -64,7 +64,7 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
+-- lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
@@ -171,6 +171,9 @@ lvim.plugins = {
   },
   {
     'sainnhe/everforest'
+  },
+  {
+    'smbl64/vim-black-macchiato'
   },
 }
 
