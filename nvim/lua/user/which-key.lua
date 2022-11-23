4383788981b131caef1a2309974c07363054ef31
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2022-11-23 12:09:56 +0800

    add search for neovim

--- nvim/lua/user/which-key.lua
+++ nvim/lua/user/which-key.lua
@@ -93,7 +93,7 @@ local mappings = {
     },
     ["<leader>w"] = {"<cmd>w<cr>", "Write"},
     ["<leader>e"] = {"<cmd>NvimTreeToggle<cr>", "File Explorer"},
-    ["<leader>c"] = { "<cmd>BufferKill<CR>", "Close Buffer" },
+    ["<leader>c"] = { "<cmd>bd<CR>", "Close Buffer" },
     ["<leader>q"] = { "<cmd>lua require('utils.functions').smart_quit()<CR>", "Quit" },
     ["<leader>f"] = { require("utils.telescope_finders").find_project_files, "Find File" },
     ["<leader>l"] = {
@@ -102,6 +102,20 @@ local mappings = {
         a = {"<cmd>Lspsaga code_action<CR>", "Code Actions"}
     },
     ["<leader>o"] = { "<cmd>LSoutlineToggle<CR>", "Show Outline"},
+    ["<leader>s"] = {
+        name = "Search",
+        b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+        c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+        f = { "<cmd>Telescope find_files<cr>", "Find File" },
+        h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+        H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+        M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+        r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+        R = { "<cmd>Telescope registers<cr>", "Registers" },
+        t = { "<cmd>Telescope live_grep<cr>", "Text" },
+        k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+        C = { "<cmd>Telescope commands<cr>", "Commands" },
+      },
     -- g related keys
     ["g"] = {
         d = {"<cmd>Lspsaga peek_definition<CR>", "Goto Definition"},
