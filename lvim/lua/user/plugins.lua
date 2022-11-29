f52f12a2862e0a63a9e85f923719d20686ede7a4
tangdi <jiaye.yang@tum.de>
2022-11-29 10:01:45 +0800

    lvim update

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -8,7 +8,7 @@ lvim.plugins = {
     end
   },
   "savq/melange",
-  {"p00f/clangd_extensions.nvim"},
+  { "p00f/clangd_extensions.nvim" },
   {
     'rose-pine/neovim',
     as = 'rose-pine',
@@ -76,7 +76,34 @@ lvim.plugins = {
       }
     end
   },
-
+  {
+    "echasnovski/mini.map",
+    branch = "stable",
+    config = function()
+      require('mini.map').setup()
+      local map = require('mini.map')
+      map.setup({
+        integrations = {
+          map.gen_integration.builtin_search(),
+          map.gen_integration.diagnostic({
+            error = 'DiagnosticFloatingError',
+            warn  = 'DiagnosticFloatingWarn',
+            info  = 'DiagnosticFloatingInfo',
+            hint  = 'DiagnosticFloatingHint',
+          }),
+        },
+        symbols = {
+          encode = map.gen_encode_symbols.dot('4x2'),
+        },
+        window = {
+          side = 'right',
+          width = 20, -- set to 1 for a pure scrollbar :)
+          winblend = 15,
+          show_integration_count = false,
+        },
+      })
+    end
+  },
   -- add markdown preview
   {
     "iamcco/markdown-preview.nvim",
