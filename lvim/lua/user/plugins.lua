b8a151774ef345f16407fa4c7fdbcb5a28a8cabc
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2022-11-29 16:24:47 +0800

    scrollbar

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -77,31 +77,9 @@ lvim.plugins = {
     end
   },
   {
-    "echasnovski/mini.map",
-    branch = "stable",
+    "petertriho/nvim-scrollbar",
     config = function()
-      require('mini.map').setup()
-      local map = require('mini.map')
-      map.setup({
-        integrations = {
-          map.gen_integration.builtin_search(),
-          map.gen_integration.diagnostic({
-            error = 'DiagnosticFloatingError',
-            warn  = 'DiagnosticFloatingWarn',
-            info  = 'DiagnosticFloatingInfo',
-            hint  = 'DiagnosticFloatingHint',
-          }),
-        },
-        symbols = {
-          encode = map.gen_encode_symbols.dot('4x2'),
-        },
-        window = {
-          side = 'right',
-          width = 20, -- set to 1 for a pure scrollbar :)
-          winblend = 15,
-          show_integration_count = false,
-        },
-      })
+      require("scrollbar").setup()
     end
   },
   -- add markdown preview
