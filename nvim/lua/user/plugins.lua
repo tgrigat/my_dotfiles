7dd24c182fc3004669314bc531cfdb7262a72c7d
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2022-12-13 23:07:14 +0800

    temporary deprecated toggleterm

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -84,50 +84,7 @@ return require('packer').startup(function(use)
   use { 'nvim-lua/popup.nvim'
   }
   use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-    require("toggleterm").setup {
-      active = true,
-      on_config_done = nil,
-      -- size can be a number or function which is passed the current terminal
-      size = 20,
-      open_mapping = [[<c-\>]],
-      hide_numbers = true, -- hide the number column in toggleterm buffers
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-      start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
-      persist_size = false,
-      -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
-      direction = "float",
-      close_on_exit = true, -- close the terminal window when the process exits
-      shell = vim.o.shell, -- change the default shell
-      -- This field is only relevant if direction is set to 'float'
-      float_opts = {
-        -- The border key is *almost* the same as 'nvim_win_open'
-        -- see :h nvim_win_open for details on borders however
-        -- the 'curved' border is a custom border type
-        -- not natively supported but implemented in this plugin.
-        -- border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-        border = "curved",
-        -- width = <value>,
-        -- height = <value>,
-        winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
-      },
-      -- Add executables on the config.lua
-      -- { exec, keymap, name}
-      -- lvim.builtin.terminal.execs = {{}} to overwrite
-      -- lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs+1] = {"gdb", "tg", "GNU Debugger"}
-      -- TODO: pls add mappings in which key and refactor this
-      execs = {
-        { vim.o.shell, "<M-1>", "Horizontal Terminal", "horizontal", 0.3 },
-        { vim.o.shell, "<M-2>", "Vertical Terminal", "vertical", 0.4 },
-        { vim.o.shell, "<M-3>", "Float Terminal", "float", nil },
-      },
-    }
+    require("user.terminal").setup()
   end }
   use 'rcarriga/nvim-notify'
   use "goolord/alpha-nvim"
@@ -156,7 +113,6 @@ return require('packer').startup(function(use)
     "windwp/nvim-autopairs",
     config = function() require("user.autopair") end
   }
-  use {}
   -- Autopairsintegrates with both cmp and treesitter
   use "williamboman/nvim-lsp-installer"
 
