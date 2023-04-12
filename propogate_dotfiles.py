c18aca1a3c0b3797aa065e17394215c0d2f2bc7d
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2023-04-12 14:46:12 +0200

    new propogate dotfile scheme

--- propogate_dotfiles.py
+++ propogate_dotfiles.py
@@ -1,5 +1,6 @@
 import os
-from typing import List, Any
+import yaml
+from typing import Dict, List, Any
 from os.path import join, isdir, isfile, islink
 import shutil
 
@@ -7,27 +8,39 @@ wkdir = os.getcwd()
 user = os.environ.get("USER")
 
 repo = wkdir
-# home = join("/home/yang/script", "P_home")  # /home/yang, for tmux and .zshrc
 home = os.environ.get("HOME")
 dot_config = f"{home}/.config"
 
-to_ignore = [".git", "dotflie.py", "confsync"]
+to_ignore = [
+    ".git",
+    "dotflie.py",
+    "confsync",
+    "destination.yaml",
+    ".gitignore",
+    "README.md",
+    "propogate_dotfiles.py",
+    ".session",
+    "kde_backup.py",
+    ".gitconfig",
+]
 
-to_home = [".zshrc", ".tmux.conf"]
 
-# TODO list
-# add .local/share
+# Read the destinations from the destination.yaml file
+def read_destinations(file: str) -> Dict[str, str]:
+    with open(file, "r") as f:
+        return yaml.safe_load(f)
 
-# read from the config directory
+
+destinations = read_destinations("destination.yaml")
+
 all_configs = [config for config in os.listdir(repo) if config not in to_ignore]
 
 
 def remove(path):
-    """param <path> could either be relative or absolute."""
     if os.path.isfile(path) or os.path.islink(path):
-        os.remove(path)  # remove the file
+        os.remove(path)
     elif os.path.isdir(path):
-        shutil.rmtree(path)  # remove dir and all contains
+        shutil.rmtree(path)
     else:
         raise ValueError("file {} is not a file or dir.".format(path))
 
@@ -35,54 +48,45 @@ def remove(path):
 def create_link(
     config_dir: str, target_dir: str, configs: List[str], safe_mode: Any = True
 ) -> None:
-
     for cfg in configs:
+        target_dir_expanded = os.path.expanduser(target_dir)
 
-        if isdir(join(target_dir, cfg)) or isfile(join(target_dir, cfg)):
-            if islink(join(target_dir, cfg)):
-                # print(f"already symlink config: {cfg}")
+        if isdir(join(target_dir_expanded, cfg)) or isfile(
+            join(target_dir_expanded, cfg)
+        ):
+            if islink(join(target_dir_expanded, cfg)):
                 continue
             else:
-                # TODO: file should be deleted or copied to the config_dir
                 if safe_mode:
                     raise NotImplementedError(
-                        f"{cfg} already exists at {target_dir}, please remove it manually"
+                        f"{cfg} already exists at {target_dir_expanded}, please remove it manually"
                     )
                 else:
-                    remove(join(target_dir, cfg))
-                    print(f"Warning: removed {cfg} in {target_dir}")
+                    remove(join(target_dir_expanded, cfg))
+                    print(f"Warning: removed {cfg} in {target_dir_expanded}")
 
-        # expect the cfg doesn't exists on the target_dir
-        os.symlink(join(config_dir, cfg), join(target_dir, cfg))
+        try:
+            os.symlink(join(config_dir, cfg), target_dir_expanded)
+        except FileExistsError:
+            print(f"Warning: {cfg} already exists at {target_dir_expanded}")
         print(f"Link {cfg} to target")
 
 
-# Scan for links that are not in repo, likely to be the one discarded. print a warning
-def scan_for_unknown(config_dir: str, configs: List) -> None:
-    for item in os.listdir(config_dir):
-        if islink(join(config_dir, item)) and item not in configs:
-            print(
-                f"Warning: {item} in ~/.config is a symlink but not in config file. Please check if this is a discarded config."
-            )
+# def scan_for_unknown(config_dir: str, configs: List) -> None:
+#     for item in os.listdir(config_dir):
+#         if islink(join(config_dir, item)) and item not in configs:
+#             print(
+#                 f"Warning: {item} in ~/.config is a symlink but not in config file. Please check if this is a discarded config."
+#             )
 
 
 for config in all_configs:
-
-    if config in to_home:
-        create_link(repo, home, [config], safe_mode=True)
+    target_dir = destinations.get(config)
+    if target_dir:
+        create_link(repo, target_dir, [config], safe_mode=False)
     else:
-        create_link(repo, dot_config, [config], safe_mode=False)
+        print(f"Warning: {config} not found in destination.yaml")
 
-# scan_for_unknown(home, all_configs)
-scan_for_unknown(dot_config, all_configs)
+# scan_for_unknown(dot_config, all_configs)
 
 print(f"finished the linking process")
-
-# check if the directory already exists on the target
-
-# create a symlink to the target
-
-# Advanced:
-# 1. check if target is already a symlink. If is, then pass (Logic for adding a new config to the folder)
-# 2. add a list of files (.zshrc, .tmux) to the different target location
-# 3. if a config is deleted in the repo, remove the symlink as well. TODO
