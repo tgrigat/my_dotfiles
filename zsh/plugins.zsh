dcc66c5b683139c07539dcfd996e2456dfd9de6f
Lumen Yang <git@lumeny.io>
2023-05-26 18:15:31 +0200

    add zap to zsh & remove svim from destination

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,3 +2,4 @@
 # export PYENV_ROOT="$HOME/.pyenv"
 # command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 # eval "$(pyenv init -)"
+[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh" && plug "blimmer/zsh-aws-vault" && plug "zsh-users/zsh-autosuggestions"
