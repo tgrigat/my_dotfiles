2a9918df8b4c49bb5170361027d16a01861b647f
Lumen Yang <lumen.yang@lumeny.io>
2023-10-24 19:25:12 +0200

    add vim slime default

--- lvim/lua/user/lvim_plugins.lua
+++ lvim/lua/user/lvim_plugins.lua
@@ -7,6 +7,7 @@ lvim.plugins = {
     config = function()
       vim.g.slime_target = "zellij"
       vim.g.slime_bracketed_paste = 1
+      vim.g.slime_default_config = {session_id = "current", relative_pane = "right"}
     end
   },
   {
