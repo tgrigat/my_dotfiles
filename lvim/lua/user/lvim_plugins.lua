a61f89ef7beefa32eb65c9196c938d8b869122aa
Lumen Yang <lumen.yang@lumeny.io>
2023-10-28 22:53:49 +0200

    reduce some noise from noice

--- lvim/lua/user/lvim_plugins.lua
+++ lvim/lua/user/lvim_plugins.lua
@@ -19,12 +19,22 @@ lvim.plugins = {
           },
         },
         -- you can enable a preset for easier configuration
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              kind = "",
+              find = "written",
+            },
+            opts = { skip = true },
+          },
+        },
         presets = {
-          bottom_search = true,   -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
+          bottom_search = true,         -- use a classic bottom cmdline for search
+          command_palette = true,       -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,     -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,       -- add a border to hover docs and signature help
         },
       })
     end,
