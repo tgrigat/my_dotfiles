32f74e2eb71e49d5f4024063f5dedb679d8ad687
Lumen Yang <lumen.yang@lumeny.io>
2023-11-10 11:25:36 +0100

    switch to flash.nvim

--- lvim/lua/user/lvim_plugins.lua
+++ lvim/lua/user/lvim_plugins.lua
@@ -87,11 +87,29 @@ lvim.plugins = {
   --     "nvim-telescope/telescope.nvim"
   --   }
   -- },
+  -- {
+  --   "ggandor/leap.nvim",
+  --   config = function()
+  --     require('leap').add_default_mappings()
+  --   end
+  -- },
+
   {
-    "ggandor/leap.nvim",
-    config = function()
-      require('leap').add_default_mappings()
-    end
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end,
+                                                                                                  desc =
+        "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc =
+      "Toggle Flash Search" },
+    },
   },
   "savq/melange",
   { "p00f/clangd_extensions.nvim" },
