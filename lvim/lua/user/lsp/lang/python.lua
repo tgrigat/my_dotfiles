7a389bfef2dc507589a74b414b865eb6913b591a
Lumen Yang <lumen.yang@lumeny.io>
2024-01-12 09:12:37 +0100

    lvim: add python ft whichkey config

--- lvim/lua/user/lsp/lang/python.lua
+++ lvim/lua/user/lsp/lang/python.lua
@@ -24,37 +24,6 @@ pcall(function()
   require("dap-python").test_runner = "pytest"
 end)
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "python" },
-  callback = function()
-    lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('dap-python').test_method()<cr>", "Test Method" }
-    lvim.builtin.which_key.mappings["df"] = { "<cmd>lua require('dap-python').test_class()<cr>", "Test Class" }
-    lvim.builtin.which_key.vmappings["d"] = {
-      name = "Debug",
-      s = { "<cmd>lua require('dap-python').debug_selection()<cr>", "Debug Selection" },
-    }
-    lvim.builtin.which_key.mappings["j"] = {
-      name = "Jupyter",
-      i = { "<Cmd>MagmaInit<CR>", "Init Magma" },
-      d = { "<Cmd>MagmaDeinit<CR>", "Deinit Magma" },
-      e = { "<Cmd>MagmaEvaluateLine<CR>", "Evaluate Line" },
-      r = { "<Cmd>MagmaReevaluateCell<CR>", "Re evaluate cell" },
-      D = { "<Cmd>MagmaDelete<CR>", "Delete cell" },
-      s = { "<Cmd>MagmaShowOutput<CR>", "Show Output" },
-      R = { "<Cmd>MagmaRestart!<CR>", "Restart Magma" },
-      S = { "<Cmd>MagmaSave<CR>", "Save" },
-    }
-    lvim.builtin.which_key.vmappings["j"] = {
-      name = "Jupyter",
-      e = { "<esc><cmd>MagmaEvaluateVisual<cr>", "Evaluate Highlighted Line" },
-    }
-    lvim.builtin.which_key.vmappings["j"] = {
-      name = "Jupyter",
-      e = { "<esc><cmd>MagmaEvaluateVisual<cr>", "Evaluate Highlighted Line" },
-    }
-  end,
-})
-
 require("lspconfig").pyright.setup {
   cmd = { "pyright-langserver", "--stdio" },
   filetypes = { "python" },
