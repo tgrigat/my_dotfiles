a3f2c2e097fa5d44e56e1f10f96bfb2aad8d54d3
Lumen Yang <lumen.yang@lumeny.io>
2024-01-25 00:15:25 +0100

    lvim: fix merge problem

--- lvim/lua/user/lvim_plugins.lua
+++ lvim/lua/user/lvim_plugins.lua
@@ -79,6 +79,8 @@ lvim.plugins = {
     -- Ensure that it runs first to minimize delay when opening file from terminal
     lazy = false,
     priority = 1001,
+  },
+  {
     "Selyss/mind.nvim",
     branch = 'v2.2',
     dependencies = {
