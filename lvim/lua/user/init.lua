f59b871089dbe207ab3230e38e45480353b99316
Lumen Yang <lumen.yang@lumeny.io>
2024-03-27 18:15:00 +0100

    formatting and cleanup

--- lvim/lua/user/init.lua
+++ lvim/lua/user/init.lua
@@ -31,7 +31,7 @@ reload("user.dap")
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
+
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
@@ -87,48 +87,49 @@ lvim.builtin.bufferline.options.max_name_length = 35
 -- })
 
 -- set onedark config
-require('onedark').setup  {
-    -- Main options --
-    style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = false,  -- Show/hide background
-    term_colors = true, -- Change terminal color as per the selected theme style
-    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+require('onedark').setup {
+  -- Main options --
+  style = 'cool',                 -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+  transparent = false,            -- Show/hide background
+  term_colors = true,             -- Change terminal color as per the selected theme style
+  ending_tildes = false,          -- Show the end-of-buffer tildes. By default they are hidden
+  cmp_itemkind_reverse = false,   -- reverse item kind highlights in cmp menu
 
-    -- toggle theme style ---
-    toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+  -- toggle theme style ---
+  toggle_style_key = nil,                                                              -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+  toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
 
-    -- Change code style ---
-    -- Options are italic, bold, underline, none
-    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-    code_style = {
-        comments = 'italic',
-        keywords = 'none',
-        functions = 'none',
-        strings = 'none',
-        variables = 'none'
-    },
+  -- Change code style ---
+  -- Options are italic, bold, underline, none
+  -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+  code_style = {
+    comments = 'italic',
+    keywords = 'none',
+    functions = 'none',
+    strings = 'none',
+    variables = 'none'
+  },
 
-    -- Lualine options --
-    lualine = {
-        transparent = false, -- lualine center bar transparency
-    },
+  -- Lualine options --
+  lualine = {
+    transparent = false,     -- lualine center bar transparency
+  },
 
-    -- Custom Highlights --
-    colors = {}, -- Override default colors
-    highlights = {}, -- Override highlight groups
+  -- Custom Highlights --
+  colors = {},       -- Override default colors
+  highlights = {},   -- Override highlight groups
 
-    -- Plugins Config --
-    diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true,   -- use undercurl instead of underline for diagnostics
-        background = true,    -- use background color for virtual text
-    },
+  -- Plugins Config --
+  diagnostics = {
+    darker = true,         -- darker colors for diagnostic
+    undercurl = true,      -- use undercurl instead of underline for diagnostics
+    background = true,     -- use background color for virtual text
+  },
 }
 
+-- Toggleterm use Ctrl v to virtual mode
 -- vim.cmd("tnoremap <C-v> <C-\\><C-n>")
-vim.api.nvim_set_keymap('t', '<C-v>', '<C-\\><C-n>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<C-v>', '<C-\\><C-n>', { noremap = true })
 
 -- Toggleterm show the terminal number
 if lvim.builtin.lualine.extensions == nil then
