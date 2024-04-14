bfd6422390279e6e103ebccdca459341aeffe568
Lumen Yang <lumen.yang@lumeny.io>
2024-04-14 19:26:21 +0200

    fix reload logic to load adapter as well

--- lvim/lua/user/keymaps/init.lua
+++ lvim/lua/user/keymaps/init.lua
@@ -182,8 +182,14 @@ lvim.builtin.which_key.mappings['dl'] = {
       return
     end
 
-    for filetype, config in pairs(project_dap_config) do
-      dap.configurations[filetype] = config
+    for key, config in pairs(project_dap_config) do
+      if key == "adapters" then
+        for language, adapter in pairs(config) do
+          dap.adapters[language] = adapter
+        end
+      else
+        dap.configurations[key] = config
+      end
     end
   end,
   "Load nvim-dap.lua"
