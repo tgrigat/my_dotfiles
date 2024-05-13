93ca62aaff69740a3d23b0d3fda7f9c0fd866614
Lumen Yang <lumen.yang@lumeny.io>
2024-05-13 16:39:24 +0200

    improve the config

--- nvim/lua/plugins/toggleterm-manager.lua
+++ nvim/lua/plugins/toggleterm-manager.lua
@@ -2,25 +2,35 @@
 return {
   {
     "ryanmsnyder/toggleterm-manager.nvim",
-    config = function()
+    opts = function(_, opts)
       local toggleterm_manager = require "toggleterm-manager"
       local actions = toggleterm_manager.actions
-      toggleterm_manager.setup {
-        mappings = { -- key mappings bound inside the telescope window
-          i = {
-            ["<CR>"] = { action = actions.toggle_term, exit_on_action = true }, -- toggles terminal open/closed
-            ["<C-i>"] = { action = actions.create_term, exit_on_action = false }, -- creates a new terminal buffer
-            ["<C-d>"] = { action = actions.delete_term, exit_on_action = false }, -- deletes a terminal buffer
-            ["<C-r>"] = { action = actions.rename_term, exit_on_action = false }, -- provides a prompt to rename a terminal
-          },
-          n = {
-            ["<CR>"] = { action = actions.toggle_term, exit_on_action = true }, -- toggles terminal open/closed
-            ["r"] = { action = actions.rename_term, exit_on_action = false }, -- provides a prompt to rename a terminal
-            ["d"] = { action = actions.delete_term, exit_on_action = false }, -- deletes a terminal buffer
-            ["n"] = { action = actions.create_term, exit_on_action = false }, -- creates a new terminal buffer
-          },
+
+      local mappings = {
+        i = {
+          ["<CR>"] = { action = actions.toggle_term, exit_on_action = true }, -- toggles terminal open/closed
+          ["<C-i>"] = { action = actions.create_term, exit_on_action = false }, -- creates a new terminal buffer
+          ["<C-d>"] = { action = actions.delete_term, exit_on_action = false }, -- deletes a terminal buffer
+          ["<C-r>"] = { action = actions.rename_term, exit_on_action = false }, -- provides a prompt to rename a terminal
+        },
+        n = {
+          ["<CR>"] = { action = actions.toggle_term, exit_on_action = true }, -- toggles terminal open/closed
+          ["r"] = { action = actions.rename_term, exit_on_action = false }, -- provides a prompt to rename a terminal
+          ["d"] = { action = actions.delete_term, exit_on_action = false }, -- deletes a terminal buffer
+          ["n"] = { action = actions.create_term, exit_on_action = false }, -- creates a new terminal buffer
         },
       }
+
+      if not opts.mappings then
+        opts.mappings = mappings
+      else
+        opts.mappings = vim.tbl_deep_extend("force", opts.mappings, mappings)
+      end
+    end,
+
+    config = function(_, opts)
+      local toggleterm_manager = require "toggleterm-manager"
+      toggleterm_manager.setup(opts)
     end,
     dependencies = {
       "akinsho/nvim-toggleterm.lua",
