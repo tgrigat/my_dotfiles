2c657b4af2bd68bd06d88349d0e908ffe2ae1edc
Lumen Yang <lumen.yang@lumeny.io>
2024-06-10 14:17:48 +0200

    nvim: vimtex viewr fix for wayland

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,13 +1,12 @@
 return {
   "lervag/vimtex",
-  lazy = false,
   dependencies = {
     "AstroNvim/astrocore",
     opts = {
       options = {
         g = {
-          -- vimtex_view_method = "zathura_simple",
-          vimtex_view_method = "sioyek",
+          vimtex_view_method = "zathura_simple",
+          -- vimtex_view_method = "sioyek",
           vimtex_callback_progpath = "/usr/bin/nvim",
         },
       },
