d20871a9ab94ad3e858dfc578f367f90c9b45e56
Lumen Yang <lumen.yang@lumeny.io>
2024-08-27 11:16:33 +0200

    nvim: gp.nvim agent update

--- nvim/lua/plugins/gp-nvim.lua
+++ nvim/lua/plugins/gp-nvim.lua
@@ -9,6 +9,49 @@ return {
       -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
     end
   end,
+  opts = {
+    agents = {
+      {
+        name = "ChatGPT3-5",
+        disable = true,
+      },
+      -- {
+      --   name = "MyCustomAgent",
+      --   provider = "copilot",
+      --   chat = true,
+      --   command = true,
+      --   model = { model = "gpt-4-turbo" },
+      --   system_prompt = "Answer any query with just: Sure thing..",
+      -- },
+      {
+        name = "Thesis Assistant",
+        chat = true,
+        command = false,
+        -- string with model name or table with model name and parameters
+        model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+        -- system prompt (use this to specify the persona/role of the AI)
+        -- system_prompt = "You are a general AI assistant.\n\n"
+        --   .. "The user provided the additional info about how they would like you to respond:\n\n"
+        --   .. "- If you're unsure don't guess and say you don't know instead.\n"
+        --   .. "- Ask question if you need clarification to provide better answer.\n"
+        --   .. "- Think deeply and carefully from first principles step by step.\n"
+        --   .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+        --   .. "- Use Socratic method to improve your thinking and coding skills.\n"
+        --   .. "- Don't elide any code from your output if the answer requires coding.\n"
+        --   .. "- Take a deep breath; You've got this!\n",
+        system_prompt = "You are an AI assistant that is good at academic writing. \n\n "
+          .. "The user need assistance in transforming his academic outline or draft into well-structured and coherent academic writing. \n\n"
+          .. "- If you decide that the request is ambiguous or unclear, please ask for clarification. \n"
+          .. "- The user is a non-native speaker, and he prefers a writing style that is accurate and easy to understand without being overly showy or exaggerated. \n"
+          .. "- Bridge the logic and expression of the user's idea if necessary \n"
+          .. "- If content is provided as bullet points, then it is a outline, please expand on the points and provide a well-structured and coherent academic writing. \n"
+          .. "- If content is provided as paragraphs, then it is a draft, please revise and improve the writing. Don't expand the draft too much unless specified otherwise.  \n"
+          .. "- When additional context are provided, % is used as the comment symbol, which indicates the overall idea of the following paragpraphs in the context. \n"
+      },
+    },
+    default_chat_agent = "Thesis Assistant",
+
+  },
   dependencies = {
     {
       "AstroNvim/astrocore",
