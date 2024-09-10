2274a0df0fbdeb4257946d6c78b34a64317acd79
Lumen Yang <lumen.yang@lumeny.io>
2024-09-10 11:14:38 +0200

    nvim: comment cleanup

--- nvim/lua/plugins/gp-nvim.lua
+++ nvim/lua/plugins/gp-nvim.lua
@@ -56,16 +56,6 @@ return {
         command = false,
         -- string with model name or table with model name and parameters
         model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
-        -- system prompt (use this to specify the persona/role of the AI)
-        -- system_prompt = "You are a general AI assistant.\n\n"
-        --   .. "The user provided the additional info about how they would like you to respond:\n\n"
-        --   .. "- If you're unsure don't guess and say you don't know instead.\n"
-        --   .. "- Ask question if you need clarification to provide better answer.\n"
-        --   .. "- Think deeply and carefully from first principles step by step.\n"
-        --   .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-        --   .. "- Use Socratic method to improve your thinking and coding skills.\n"
-        --   .. "- Don't elide any code from your output if the answer requires coding.\n"
-        --   .. "- Take a deep breath; You've got this!\n",
         system_prompt = "You are an AI programming assistant. Follow the user's requirements carefully and to the letter. First, think step-by-step and describe your plan for what to build in pseudocode, written out in great detail. Then, output the code in a single code block. Minimize any other prose. \n\n ",
       },
     },
