379d29c34ad191ff5dfd0314505f90cbc777a4f5
Lumen Yang <lumen.yang@lumeny.io>
2024-09-25 09:08:48 +0200

    nvim: add aider nvim integration

---
+++ nvim/lua/plugins/aider-nvim.lua
@@ -0,0 +1,34 @@
+local Terminal = require("toggleterm.terminal").Terminal
+local aider = Terminal:new {
+  cmd = "aider",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<esc>", "<C-\\><C-n>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-v>", "<C-\\><C-n>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-h>", "<cmd>lua _G.AIDER_TOGGLE()<CR>", {noremap = true, silent = true})
+  end,
+}
+
+_G.AIDER_TOGGLE = function() aider:toggle() end
+
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {},
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<leader>ta"] = { "<cmd>lua _G.AIDER_TOGGLE()<CR>", desc = "Toggle Aider" },
+        },
+      },
+    },
+  },
+}
