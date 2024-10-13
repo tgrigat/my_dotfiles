e3137ba3e6695f4cb48685ebc6bbeee18d6f4457
Lumen Yang <lumen.yang@lumeny.io>
2024-10-14 00:22:43 +0200

    aider: switch to deepseek

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,5 +4,6 @@ export AIDER_DARK_MODE=true
 export AIDER_AUTO_COMMITS=false
 export AIDER_GITIGNORE=false
 export AIDER_VIM=true
-export AIDER_SONNET=true
+# export AIDER_SONNET=true
+export AIDER_DEEPSEEK=true
 export AIDER_SUGGEST_SHELL_COMMANDS=false
