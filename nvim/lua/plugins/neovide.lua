4815052b5a7fa0cc5e23debd55dbceb4122674ce
Lumen Yang <lumen.yang@lumeny.io>
2024-10-14 12:14:23 +0200

    nvim: neovide config adapted for the remote server

--- nvim/lua/plugins/neovide.lua
+++ nvim/lua/plugins/neovide.lua
@@ -1,93 +1,17 @@
-if not vim.g.neovide then return {} end
-
--- vim.o.guifont = "Iosevka Nerd Font Mono:h12"
-
 return {
-  opts = {
-    options = {
-      o = {
-        guifont = "Iosevka Nerd Font Mono:h12",
-        -- neovide_input_ime = true
-      },
-      g = {
-        neovide_cursor_animation_length = 0.07,
-        neovide_cursor_trail_size = 0.6,
-        neovide_cursor_smooth_blink = true,
-        neovide_scroll_animation_length = 0.2,
-        neovide_input_ime = true,
-        neovide_transparency = 0.95,
-        neovide_cursor_vfx_mode = "railgun"
-      },
-      -- g = { clipboard = "unnamedplus" },
-    },
   {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
     opts = {
-      mappings = {
-        n = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (normal mode)",
-          },
-        },
-        v = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (visual mode)",
-          },
-        },
-        s = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (select mode)",
-          },
-        },
-        x = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (visual block mode)",
-          },
-        },
-        o = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (operator-pending mode)",
-          },
-        },
-        i = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (insert mode)",
-          },
-        },
-        l = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (language-specific mode)",
-          },
-        },
-        c = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (command-line mode)",
-          },
-        },
-        t = {
-          ["<C-S-V>"] = {
-            function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
-            desc = "Paste from clipboard (terminal mode)",
-          },
-        },
-      },
       autocmds = {
-        neovide_options = {
+        neovide_config = {
           {
-            event = "VimEnter",
-            desc = "Set options for neovide",
+            event = { "UIEnter", "FocusGained" },
+            desc = "Apply Neovide configuration",
             callback = function()
               if vim.g.neovide then
-                vim.opt.guifont = "Iosevka Nerd Font Mono:h12"
+                -- Options
+                vim.o.guifont = "Iosevka Nerd Font Mono:h12"
                 vim.g.neovide_cursor_animation_length = 0.07
                 vim.g.neovide_cursor_trail_size = 0.6
                 vim.g.neovide_cursor_smooth_blink = true
@@ -95,6 +19,14 @@ return {
                 vim.g.neovide_input_ime = true
                 vim.g.neovide_transparency = 0.95
                 vim.g.neovide_cursor_vfx_mode = "railgun"
+
+                -- Keymaps
+                local function paste_from_clipboard() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end
+
+                local modes = { "n", "v", "s", "x", "o", "i", "l", "c", "t" }
+                for _, mode in ipairs(modes) do
+                  vim.keymap.set(mode, "<C-S-V>", paste_from_clipboard, { desc = "Paste from clipboard" })
+                end
               end
             end,
           },
