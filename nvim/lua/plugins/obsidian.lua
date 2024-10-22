90c230babc78c43ba699d048530bb55b86c2c9fb
Lumen Yang <lumen.yang@lumeny.io>
2024-10-22 21:09:29 +0200

    nvim: improve obsidian plugin

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,11 +1,32 @@
--- if true then return {} end
+local workspaces = {
+  { name = "public", path = "~/Documents/git/Notes/content", strict = true },
+  { name = "private", path = "~/obsidian/" },
+}
+
+local function workspace_exists(workspace) return vim.fn.isdirectory(vim.fn.expand(workspace.path)) == 1 end
+
+local any_workspace_exists = false
+for _, workspace in ipairs(workspaces) do
+  if workspace_exists(workspace) then
+    any_workspace_exists = true
+    break
+  end
+end
+
+if not any_workspace_exists then return {} end
 
 return {
   "epwalsh/obsidian.nvim",
-  -- the obsidian vault in this default config  ~/obsidian-vault
-  -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
-  -- event = { "BufReadPre " .. vim.fn.expand "~" .. "/obsidian/**.md" },
-  -- event = { "BufReadPre  */obsidian/*.md" },
+  -- Event for all defined workspaces
+  event = function()
+    local events = {}
+    for _, workspace in ipairs(workspaces) do
+      if workspace_exists(workspace) then
+        table.insert(events, "BufReadPre " .. vim.fn.expand(workspace.path) .. "/*.md")
+      end
+    end
+    return events
+  end,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
@@ -13,6 +34,11 @@ return {
     {
       "AstroNvim/astrocore",
       opts = {
+        options = {
+          opt = {
+            conceallevel = 1, -- sets vim.opt.conceallevel to 1
+          },
+        },
         mappings = {
           n = {
             ["<Leader>O"] = { name = "Obsidian" },
@@ -40,32 +66,113 @@ return {
       },
     },
   },
-  opts = {
-    dir = vim.env.HOME .. "/obsidian", -- specify the vault location. no need to call 'vim.fn.expand' here
-    use_advanced_uri = true,
-    finder = "telescope.nvim",
-
-    templates = {
-      subdir = "templates",
-      date_format = "%Y-%m-%d-%a",
-      time_format = "%H:%M",
-    },
-
-    note_frontmatter_func = function(note)
-      -- This is equivalent to the default frontmatter function.
-      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
+  opts = function(_, opts)
+    -- opts parameter is the default options table
+    -- overwrite the entire opts table
+    return {
+      use_advanced_uri = true,
+      finder = "telescope.nvim",
+      workspaces = workspaces,
+      -- templates = {
+      --   subdir = "templates",
+      --   date_format = "%Y-%m-%d-%a",
+      --   time_format = "%H:%M",
+      -- },
+      open_app_foreground = true,
+      note_frontmatter_func = function(note)
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+        if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
         end
-      end
-      return out
-    end,
+        return out
+      end,
+      mappings = {
+        -- ["gf"] = {
+        --   action = function() return require("obsidian").util.gf_passthrough() end,
+        --   opts = { noremap = false, expr = true, buffer = true },
+        -- },
+        -- ["<leader>ch"] = {
+        --   action = function() return require("obsidian").util.toggle_checkbox() end,
+        --   opts = { buffer = true },
+        -- },
+        ["<cr>"] = {
+          action = function() return require("obsidian").util.smart_action() end,
+          opts = { buffer = true, expr = true },
+        },
+      },
+      follow_url_func = vim.ui.open or function(url) require("astrocore").system_open(url) end,
+      -- Optional, configure additional syntax highlighting / extmarks.
+      -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+      ui = {
+        enable = true, -- set to false to disable all additional syntax features
+        update_debounce = 200, -- update delay after a text change (in milliseconds)
+        max_file_length = 5000, -- disable UI features for files with more than this many lines
+        -- Define how various check-boxes are displayed
+        checkboxes = {
+          -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "", hl_group = "ObsidianDone" },
+          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+          ["!"] = { char = "", hl_group = "ObsidianImportant" },
+          -- Replace the above with this if you don't have a patched font:
+          -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+          -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
 
-    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
-    -- URL it will be ignored but you can customize this behavior here.
-    follow_url_func = vim.ui.open or function(url) require("astrocore").system_open(url) end,
-  },
+          -- You can also add more custom ones...
+        },
+        -- Use bullet marks for non-checkbox lists.
+        bullets = { char = "•", hl_group = "ObsidianBullet" },
+        external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        -- Replace the above with this if you don't have a patched font:
+        -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = "ObsidianRefText" },
+        highlight_text = { hl_group = "ObsidianHighlightText" },
+        tags = { hl_group = "ObsidianTag" },
+        block_ids = { hl_group = "ObsidianBlockID" },
+        hl_groups = {
+          -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+          ObsidianTodo = { bold = true, fg = "#f78c6c" },
+          ObsidianDone = { bold = true, fg = "#89ddff" },
+          ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+          ObsidianTilde = { bold = true, fg = "#ff5370" },
+          ObsidianImportant = { bold = true, fg = "#d73128" },
+          ObsidianBullet = { bold = true, fg = "#89ddff" },
+          ObsidianRefText = { underline = true, fg = "#c792ea" },
+          ObsidianExtLinkIcon = { fg = "#c792ea" },
+          ObsidianTag = { italic = true, fg = "#89ddff" },
+          ObsidianBlockID = { italic = true, fg = "#89ddff" },
+          ObsidianHighlightText = { bg = "#75662e" },
+        },
+      },
+
+      -- Specify how to handle attachments.
+      attachments = {
+        -- The default folder to place images in via `:ObsidianPasteImg`.
+        -- If this is a relative path it will be interpreted as relative to the vault root.
+        -- You can always override this per image by passing a full path to the command instead of just a filename.
+        img_folder = "assets/imgs", -- This is the default
+
+        -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+        ---@return string
+        img_name_func = function()
+          -- Prefix image names with timestamp.
+          return string.format("%s-", os.time())
+        end,
+
+        -- A function that determines the text to insert in the note when pasting an image.
+        -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+        -- This is the default implementation.
+        ---@param client obsidian.Client
+        ---@param path obsidian.Path the absolute path to the image file
+        ---@return string
+        img_text_func = function(client, path)
+          path = client:vault_relative_path(path) or path
+          return string.format("![%s](%s)", path.name, path)
+        end,
+      },
+    }
+  end,
 }
