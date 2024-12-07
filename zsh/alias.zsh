d67154b22d1cf0cb0006f195335d5e5c7815e934
Lumen Yang <lumen.yang@lumeny.io>
2024-12-07 16:05:19 +0100

    zsh: add uvsh alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -101,6 +101,30 @@ alias cppcourse="cd /home/yang/Desktop/CPP && vf "
 # for helping zotero get rid of enforced dark theme 
 alias zotero="GTK_THEME=Default zotero "
 
+uvsh() {
+    local venv_name=${1:-'.venv'}
+    venv_name=${venv_name//\//} # remove trailing slashes (Linux)
+    venv_name=${venv_name//\\/} # remove trailing slashes (Windows)
+
+    local venv_bin=
+    if [[ -d ${WINDIR} ]]; then
+        venv_bin='Scripts/activate'
+    else
+        venv_bin='bin/activate'
+    fi
+
+    local activator="${venv_name}/${venv_bin}"
+    echo "[INFO] Activate Python venv: ${venv_name} (via ${activator})"
+
+    if [[ ! -f ${activator} ]]; then
+        echo "[ERROR] Python venv not found: ${venv_name}"
+        return
+    fi
+
+    # shellcheck disable=SC1090
+    . "${activator}"
+}
+
 if [[ "${SHELL##*/}" = "zsh" ]]; then
   alias refresh-fzf="rm ~/.local/bin/fzf && exec zsh"
 fi
