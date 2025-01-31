82add6e1b0cb654dd144364194fdbba908fdebb9
Lumen Yang <lumen.yang@lumeny.io>
2025-02-01 00:19:08 +0100

    fish: auto os detection

---
+++ fish/conf.d/tide_auto_os.fish
@@ -0,0 +1,27 @@
+function tide_auto_os
+    switch (uname)
+        case Darwin
+            echo '' # Apple icon
+        case Linux
+            # Check for specific Linux distributions
+            if test -f /etc/os-release
+                set -l os_id (awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '"')
+                switch $os_id
+                    case ubuntu
+                        echo '' # Ubuntu icon
+                    # case debian
+                    #     echo '' # Debian icon
+                    case arch
+                        echo '' # Arch icon
+                    case '*'
+                        echo '' # Generic Linux icon
+                end
+            else
+                echo '' # Generic Linux icon
+            end
+        case '*'
+            echo '' # Generic OS icon
+    end
+end
+
+set -x tide_os_icon (tide_auto_os)
