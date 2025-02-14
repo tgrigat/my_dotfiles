e5c46b829550ef6e9c3a00e0c970207fa36ce05b
Lumen Yang <git@lumeny.io>
2025-02-14 22:44:37 +0100

    fish: paper to markdown

---
+++ fish/functions/paper2md.fish
@@ -0,0 +1,31 @@
+function paper2md
+    # Select PDF via fzf
+    set -l pdf_file (fd -e pdf | fzf --height 40% --reverse --query="Zotero/storage/")
+    if test -z "$pdf_file"
+        echo "No file selected"
+        return
+    end
+
+    # Create temporary output directory
+    set -l tmp_output "/tmp/md"
+    mkdir -p $tmp_output
+
+    # Generate base name for output files
+    set -l base_name (basename $pdf_file .pdf | string replace -r " \\d{4}- " " ")
+
+    # Process with marker_single
+    marker_single "$pdf_file" --output_dir $tmp_output
+
+    # Move the markdown file to Zotero papermd
+    set -l md_output "$HOME/Zotero/papermd"
+    mkdir -p $md_output
+    
+    if test -e "$tmp_output/$base_name/$base_name.md"
+        mv -v "$tmp_output/$base_name/$base_name.md" "$md_output/"
+        mv -v "$tmp_output/$base_name/$base_name_meta.json" "$md_output/"
+        echo "Files moved to $md_output"
+        rm -rfv "$tmp_output/$base_name"
+    else
+        echo "Conversion failed - no markdown file found"
+    end
+end
