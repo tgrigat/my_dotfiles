3febade7e9f98111a459c1d77a96155ebc26e841
Lumen Yang <lumen.yang@lumeny.io>
2025-04-09 17:30:56 +0200

    fish: thing

--- fish/conf.d/aider.fish
+++ fish/conf.d/aider.fish
@@ -9,7 +9,7 @@ set -x AIDER_VIM true
 set -x AIDER_SUGGEST_SHELL_COMMANDS false
 set -x AIDER_SHOW_RELEASE_NOTES false
 set -x AIDER_ARCHITECT true
-set -x AIDER_MODEL openrouter/deepseek/deepseek-r1
+set -x AIDER_MODEL openrouter/deepseek/deepseek-chat-v3-0324
 set -x AIDER_EDITOR_MODEL openrouter/deepseek/deepseek-chat-v3-0324
 set -x AIDER_CACHE_PROMPTS true
 # alias aider="aider --model openrouter/anthropic/claude-3.5-sonnet"
