5bdb84cc31d655645665a79d2939b67d6663d9d0
Lumen Yang <lumen.yang@lumeny.io>
2025-04-24 16:30:38 +0200

    aider: edit format

--- fish/conf.d/aider.fish
+++ fish/conf.d/aider.fish
@@ -10,6 +10,7 @@ set -x AIDER_SUGGEST_SHELL_COMMANDS false
 set -x AIDER_SHOW_RELEASE_NOTES false
 set -x AIDER_ARCHITECT false
 set -x AIDER_MODEL openrouter/google/gemini-2.5-pro-preview-03-25
+set -x AIDER_EDIT_FORMAT diff-fenced
 # set -x AIDER_EDITOR_MODEL openrouter/deepseek/deepseek-chat-v3-0324
 set -x AIDER_CACHE_PROMPTS true
 # alias aider="aider --model openrouter/anthropic/claude-3.5-sonnet"
