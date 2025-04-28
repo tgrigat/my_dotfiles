69450680ac4609dc0d05fa206372cbce381d8d47
Lumen Yang <lumen.yang@lumeny.io>
2025-04-28 17:09:43 +0200

    chore: comment the debug dir out

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -1,13 +1,14 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
 return {
-  -- { dir = "/home/yang/Documents/git/astrocommunity" },
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+    -- { dir = "/home/yang/Documents/git/astrocommunity" },
   -- { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.bash" },
