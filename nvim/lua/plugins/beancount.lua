f04bc2d175cb771c45ea6664d100b422080e0ccc
Lumen Yang <lumen.yang@lumeny.io>
2025-04-28 22:10:57 +0200

    nvim: fix for beancount

--- nvim/lua/plugins/beancount.lua
+++ nvim/lua/plugins/beancount.lua
@@ -1,28 +1,32 @@
 -- return {} if not on hostname node
-if vim.fn.hostname() ~= "node" then return {} end
+if vim.fn.hostname() ~= "pocket" then return {} end
 
 local beancount_file = "/home/yang/Documents/git/finance/beans/main.bean"
 
 return {
   {
-    "saghen/blink.compat",
+    "Saghen/blink.cmp",
     -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    specs = {
+      { "Saghen/blink.compat", version = "*", lazy = true, opts = {} },
+    },
+    dependencies = {
+      "crispgm/cmp-beancount",
+    },
     version = "*",
-    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-    lazy = true,
     -- make sure to set opts so that lazy.nvim calls blink.compat's setup
     opts = {
       sources = {
-        default = {"breacount"},
+        default = { "beancount" },
         providers = {
           beancount = {
             name = "beancount",
             module = "blink.compat.source",
-            option = {
+            opts = {
               account = beancount_file,
-            }
-          }
-        }
+            },
+          },
+        },
       },
     },
   },
@@ -35,13 +39,8 @@ return {
       opts.servers = opts.servers or {}
       table.insert(opts.servers, "beancount")
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      -- Add com_nvim_lsp capabilities
-      local cmp_lsp = require "cmp_nvim_lsp"
-      local cmp_lsp_capabilities = cmp_lsp.default_capabilities()
-      capabilities = vim.tbl_deep_extend("keep", capabilities, cmp_lsp_capabilities)
-      -- Add any additional plugin capabilities here.
-      -- Make sure to follow the instructions provided in the plugin's docs.
+      -- Removed the manual require("cmp_nvim_lsp") and capability merging.
+      -- AstroLSP will provide the necessary capabilities, including those from cmp-nvim-lsp if available.
 
       -- extend our configuration table to include `beancount`
       opts.config = require("astrocore").extend_tbl(opts.config or {}, {
@@ -51,7 +50,6 @@ return {
             journal_file = beancount_file,
           },
           filetypes = { "beancount" },
-          capabilities = capabilities,
         },
       })
     end,
