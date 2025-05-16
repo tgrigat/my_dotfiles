dc432b579f89a8a42445358058e549b40c7ddfff
Lumen Yang <lumen.yang@lumeny.io>
2025-05-16 15:41:17 +0200

    nvim: snack animate enable

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,12 +1,14 @@
 -- lazy.nvim
 return {
   "folke/snacks.nvim",
-  ---@type snacks.Config
   opts = {
     image = {
       -- your image configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
+    scroll = { enabled = true },
+    indent = { animate = { enabled = true }},
+    dim = { animate = {enabled = true }}
   },
 }
